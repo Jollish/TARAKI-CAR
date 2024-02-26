874d41ece39003574a87de80b2ac37c9be06ef0a
Joseph <49267175+joseph-deluna@users.noreply.github.com>
2024-02-26 15:15:38 +0800

    Database icons added

--- taraki-car/src/components/abt_content/Abt_content.jsx
+++ taraki-car/src/components/abt_content/Abt_content.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './abt_content.css';
 
-const Abt_content = ({ title, text }) => (
+
+const Abt_content = ({ title, text, icon }) => (
   <div className="abt_content-container">
     <div className="abt_content-container-title">
       <div />
+      <span className='db_icon'>{icon}</span>
       <h1>{title}</h1>
     </div>
     <div className="abt_content-container-text">
--- taraki-car/src/container/database/Database.jsx
+++ taraki-car/src/container/database/Database.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Abt_content from '../../components/abt_content/Abt_content';
 import './database.css';
+import { ImBubbles } from "react-icons/im";
+import { ImInfo } from "react-icons/im";
+import { ImUserTie } from "react-icons/im";
+import { ImBriefcase } from "react-icons/im";
+import { ImOffice } from "react-icons/im";
+import { ImStack } from "react-icons/im";
+import { ImUsers } from "react-icons/im";
 
 const Database = () => (
   <div className="database section__margin" id="db">
@@ -13,13 +20,13 @@ HEI Researchers" />
       <p>Explore the Library</p>
     </div>
     <div className="database-container">
-      <Abt_content title="HEI Researchers"/>
-      <Abt_content title="Negosyo Centers"/>
-      <Abt_content title="Business Mentors"/>
-      <Abt_content title="Industries"/>
-      <Abt_content title="Higher Education Institutions"/>
-      <Abt_content title="R & D Facilities"/>
-      <Abt_content title="Shared Services Facilites"/>
+      <Abt_content icon={<ImInfo />} title="HEI Researchers"/>
+      <Abt_content icon={<ImBubbles />} title="Negosyo Centers"/>
+      <Abt_content icon={<ImUserTie />} title="Business Mentors"/>
+      <Abt_content icon={<ImBriefcase />} title="Industries"/>
+      <Abt_content icon={<ImOffice />} title="Higher Education Institutions"/>
+      <Abt_content icon={<ImStack />} title="R & D Facilities"/>
+      <Abt_content icon={<ImUsers />} title="Shared Services Facilites"/>
     </div>
   </div>
 );
